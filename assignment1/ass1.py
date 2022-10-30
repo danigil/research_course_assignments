4ca956cb7e8c17612687e58961797e7172b08b28
Daniel Gilkarov <dgilkarov33@gmail.com>
2022-10-30 14:22:52 +0200

    Made progress in 3rd question

--- assignment1/ass1.py
+++ assignment1/ass1.py
@@ -1,6 +1,5 @@
 from doctest import testmod
 from queue import Queue
-from typing import Tuple
 
 
 def f1(x: int, y: int, z):
@@ -8,21 +7,29 @@ def f1(x: int, y: int, z):
 
 
 def safe_call(func, *args):
-    """safe_call receives function, args for that function and calls the function
-     with those args if they match the annotations, otherwise throws exception.
+    """
+    safe_call receives function, args for that function and calls the function
+    with those args if they match the annotations, otherwise throws exception.
 
-    #Examples where the function will work correctly:
-    >>> safe_call(f1,5,5,5)
-    15
+    ##Good Cases (Shouldn't throw)
+        >>> safe_call(f1,5,5,5)
+        15
 
-    >>> safe_call(f1,5,5,5.7)
-    15.7
+        >>> safe_call(f1,5,5,5.7)
+        15.7
 
-    #Example where the function should throw an exception:
-    >>> safe_call(f1,5.0,"hello",'z')
-    Traceback (most recent call last):
-     ...
-    Exception
+    ##Bad Cases (Should throw)
+        #mismatching annotation
+        >>> safe_call(f1,5.0,"hello",'z')
+        Traceback (most recent call last):
+         ...
+        Exception
+
+        #incorrect order
+        >>> safe_call(f1,"1",1,1)
+        Traceback (most recent call last):
+         ...
+        Exception
      """
 
     for arg, annot in zip(func.__annotations__.values(), (type(val) for val in args)):
@@ -37,7 +44,19 @@ def four_neighbor_function(node):
     return [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
 
 
-def breadth_first_search(start: Tuple[int, ...], end: Tuple[int, ...], func):
+def breadth_first_search(start, end, func):
+    """
+    breadth_first_search
+    receives start node, end node, function that returns neighbours of a node
+    performs BFS search when a path from start to end is found, it is printed and the function ends.
+
+    the function is implemented with the assumption that == operator,__repr__ exists for start and end objects
+
+    ##Good Cases (Shouldn't throw)
+        >>> breadth_first_search((0,0),(1,3),four_neighbor_function)
+        (0, 0) -> (0, 1) -> (0, 2) -> (0, 3) -> (1, 3)
+
+    """
     predecessors = {}
 
     Q = Queue()
@@ -59,16 +78,54 @@ def breadth_first_search(start: Tuple[int, ...], end: Tuple[int, ...], func):
 
     def print_path(start, end):
         if start == end:
-            print(start)
+            print(start,end="")
         elif end not in predecessors:
             print("no path")
         else:
             print_path(start, predecessors[end])
-            print(end)
+            print(" ->",end,end="")
 
     print_path(start, end)
 
 
+def print_sorted(obj):
+    def print_sorted_list(l:list):
+        print(sorted(l))
+    def print_sorted_dict(d:dict):
+        print("dict")
+    def print_sorted_set(s:set):
+        print("set")
+    def print_sorted_tuple(t:tuple):
+        print("tuple")
+
+    funcs_dict = {
+        list: print_sorted_list,
+        dict: print_sorted_dict,
+        set: print_sorted_set,
+        tuple: print_sorted_tuple
+    }
+
+    funcs_dict.get(type(obj),print)(obj)
+
+
 if __name__ == '__main__':
-    # testmod(name='safe_call', verbose=True)
-    breadth_first_search((0, 0), (1, 3), four_neighbor_function)
+    x = {"a" : 5, "c" : 6, "b" : [1,3,2,4]}
+    l= [1,3,2]
+    print_sorted(l)
+    print(l)
+    """"
+    s = str(x)
+    print(s)
+    
+    print(sorted(x))
+    
+    print_sorted(x)
+    print(list(x.items()))
+    l = list(x.items())
+    l.sort(key= lambda a:a[0])
+    
+    print(l)
+"""
+    #testmod(name='assignment1', verbose=True)
+    #testmod(name='breadth', verbose=True)
+    #breadth_first_search((0, 0), (1, 3), four_neighbor_function)
